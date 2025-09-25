f37002d5d03506678869ed6298b9ff7a07f733e5
Kameramann5 <d.lisachyov2013@yandex.ru>
2025-09-25 15:31:34 +0300

    prod

--- crypto/src/App.vue
+++ crypto/src/App.vue
@@ -1,11 +1,108 @@
-<script>
-
-</script>
-
 <template>
+<h1>Crypto</h1>
+<Input :changeAmount="changeAmount" :convert="convert"  :favourite="favourite" />
+<p v-if="error!='' ">{{ error }}</p>
+<p v-if="result!=0" className="result-text">{{ result }}</p>
+<Favourite :favs="favs" v-if="favs.length > 0" :getFromFavs="getFromFavs" />
+<div className="selectors">
+    <Selector :setCrypto="setcryptoFirst" :cryptoNow="cryptoFirst" />
+    <Selector :setCrypto="setcryptoSecond" :cryptoNow="cryptoSecond" />
+</div>
 
 </template>
 
+<script>
+import Input from './components/input.vue';
+import Selector from './components/selector.vue';
+import Favourite from './components/Favourite.vue';
+import CryptoConvert from 'crypto-convert';
+
+const convert = new CryptoConvert();
+
+
+
+
+export default {
+    components: { Input,Selector, Favourite},
+    data() {
+        return {
+            amount:0,
+            cryptoFirst:'',
+            cryptoSecond:'',
+            error:'',
+            result:0,
+            favs: []
+        }
+    },
+    methods: {
+        favourite(val) {
+            this.favs.push({
+from: this.cryptoFirst,
+to:this.cryptoSecond
+            });
+        },
+getFromFavs(index) {
+this.cryptoFirst=this.favs[index].from
+this.cryptoSecond=this.favs[index].to
+},
+        changeAmount(val) {
+            this.amount=val
+        },
+        setcryptoFirst(val) {
+            this.cryptoFirst=val
+        },
+        setcryptoSecond(val) {
+            this.cryptoSecond=val
+        },
+        async convert() {
+if(this.amount<=0) {
+    this.error='Введите число больше 0';
+    return;
+}
+else if(this.cryptoFirst=='' || this.cryptoSecond=='') {
+    this.error='Выберите валюту';
+    return;
+}
+else if(this.cryptoFirst==this.cryptoSecond) {
+    this.error='Выберите разные валюты';
+    return;
+}
+
+this.error='';
+
+ await convert.ready();
+
+if(this.cryptoFirst=='BTC' && this.cryptoSecond=='ETH') {
+    this.result= convert.BTC.ETH(this.amount);
+}
+else  if(this.cryptoFirst=='BTC' && this.cryptoSecond=='USDT') {
+    this.result= convert.BTC.USDT(this.amount);
+}
+else  if(this.cryptoFirst=='ETH' && this.cryptoSecond=='BTC') {
+    this.result= convert.BTC.BTC(this.amount);
+}
+else  if(this.cryptoFirst=='ETH' && this.cryptoSecond=='USDT') {
+    this.result= convert.BTC.USDT(this.amount);
+}
+else  if(this.cryptoFirst=='USDT' && this.cryptoSecond=='BTC') {
+    this.result= convert.USDT.BTC(this.amount);
+}
+else  if(this.cryptoFirst=='USDT' && this.cryptoSecond=='ETH') {
+    this.result= convert.USDT.ETH(this.amount);
+}
+
+        }
+    }
+}
+</script>
+
+
 <style scoped>
 
+.selectors {
+display: flex;
+justify-content: space-around;
+width: 700px;
+margin: 0 auto;
+}
 </style>
---
+++ crypto/src/components/input.vue
@@ -0,0 +1,58 @@
+<template>
+<input min="0" type="number" placeholder="Введите число" @input="changeAmount($event.target.value)">
+<br>
+<button @click="convert()">Конвертировать</button>
+<button @click="favourite()" className="fav">В избранное</button>
+</template>
+
+
+
+
+<script>
+export default {
+props: {
+    changeAmount: {
+        type: Function,
+        required: true,
+    },
+    convert: {
+        type: Function,
+        required: true,
+    },
+    favourite: {
+        type: Function,
+        required: true,
+    },
+}
+}
+</script>
+
+<style scoped>
+.fav {
+    margin-left: 20px;
+}
+input {
+    border-radius: 3px;
+    border: 0;
+    padding: 10px 15px;
+    outline: none;
+    color: #333;
+    background: #fafafa;
+    width: 500px;
+    position: relative;
+    top: -50px;
+    font-size: 1.2em;
+
+}
+button { 
+    position: relative;
+    top: -20px;
+    padding: 15px 20px;
+    color: white;
+    text-transform: uppercase;
+    cursor: pointer;
+    background:black;
+    outline: none;
+    border: none;
+}
+</style>
